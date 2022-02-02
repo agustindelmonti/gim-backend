c95bec418fcf1213e7b246920e01f5002d365448
Agustin <agus.delmonti@gmail.com>
2022-02-02 20:00:48 -0300

    auth authentication config

---
+++ src/main/java/gym/security/CustomAuthenticationFilter.java
@@ -0,0 +1,71 @@
+package gym.security;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Autowired
+    private Environment env;
+
+    /**
+     * Se ejecuta siempre que un usuario intente autenticarse
+     */
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        var token = new UsernamePasswordAuthenticationToken(username, password);
+        return authenticationManager.authenticate(token);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+
+        User user = (User) authResult.getPrincipal();
+        String secret = env.getProperty("secret");
+        Algorithm algorithm = Algorithm.HMAC256(secret.getBytes());
+
+        String access_token = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList())
+                .sign(algorithm);
+
+        String refresh_token = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 60 * 60 * 1000))
+                .withIssuer(request.getRequestURL().toString())
+                .sign(algorithm);
+
+        response.setHeader("access_token", access_token);
+        response.setHeader("refresh_token", refresh_token);
+    }
+
+}
--- src/main/java/gym/services/UserService.java
+++ src/main/java/gym/services/UserService.java
@@ -6,14 +6,23 @@ import gym.model.User;
 import gym.repository.RoleRepository;
 import gym.repository.UserRepository;
 import gym.utils.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class UserService implements IUserService {
+@Slf4j
+public class UserService implements IUserService, UserDetailsService {
     @Autowired
     UserRepository userRepository;
 
@@ -43,6 +52,19 @@ public class UserService implements IUserService {
         User user = userRepository.findByEmail(email);
         final Role role = roleRepository.findByName(roleName);
         user.getRoles().add(role);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        final User user = userRepository.findByEmail(username);
+        if (user == null) {
+            log.error("No se encontró un usuario registrado con email {}", username);
+            throw new UsernameNotFoundException("No se encontró un usuario registrado");
+        }
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(r -> authorities.add(new SimpleGrantedAuthority(r.name)));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
     }
 
 }
